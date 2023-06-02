46efcd850a3a1724195299689d4b21f640d6bda7
Msole18 <miguel.sole18@gmail.com>
2023-06-02 16:10:23 -0300

    adding modal

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import classes from "./Cart.module.css";
+import { Modal }  from "../UI/Modal";
 
-const Cart = (props) => {
+export const Cart = (props) => {
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "Cachapa", amount: 2, price: 12.99 }].map(
@@ -9,18 +10,18 @@ const Cart = (props) => {
     </ul>
   );
   return (
-    <div>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
+        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
-};
+}; 
 
-export default Cart;
+
